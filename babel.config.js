62d05cf66943a245e10fa370c6f4b3d9e7102fa2
Maykon Sousa <maykon.sousa@hotmail.com>
2023-09-25 08:05:12 -0300

    chore: initial config

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+    [
+      'module-resolver',
+    {
+      root: ['./src'],
+      alias: {
+        "@dtos": "./src/dtos",
+        "@models": "./src/models",
+        "@services": "./src/services",
+        "@utils": "./src/utils",
+        "@screens": "./src/screens",
+        "@components": "./src/components",
+        "@navigation": "./src/navigation",
+        "@hooks": "./src/hooks",
+        "@contexts": "./src/contexts",
+        "@assets": "./src/assets",
+        "@routes": "./src/routes",
+
+      }
+    }
+    ]
+    ],
   };
 };
