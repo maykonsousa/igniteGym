b6814566a91b271728a2007624a38c3483e761b4
Maykon Sousa <maycon.sousa@nutrien.com>
2023-10-07 13:37:33 -0300

    feat: include Logo

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import {VStack, Image} from 'native-base'
+import {VStack, Image, Text, Center} from 'native-base'
 import BackgroundImg from '@assets/background.png'
-
+import LogoSVG from '@assets/logo.svg'
 export const SignIn = () => {
   return (
-    <VStack>
+    <VStack flex={1} bg="gray.700">
       <Image 
       source={BackgroundImg} 
       alt="Imagem de fundo com pessoas treinando"
       resizeMode='contain'
       position={'absolute'}
        />
+       <Center my={24}>
+        <LogoSVG  />
+        <Text color="white">Treine sua mente e seu corpo</Text>
+       </Center>
     </VStack>
   )
 }
